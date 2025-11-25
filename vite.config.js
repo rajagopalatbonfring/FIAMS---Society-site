8df32d2079c42b47cc7f21d24f1336694398ebcb
rajagopalatbonfring <researchteambts6@gmail.com>
2025-11-25 17:28:29 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // ← important for Render
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
